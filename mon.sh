aa6a31e0b28a5f31e82f6dff580e356e6ee4cfbf
Vu Tran <baysao@gmail.com>
2016-06-09 23:52:52 +0700

    add mon

---
+++ mon.sh
@@ -0,0 +1,2 @@
+watch './tool.sh db_c;echo -n "redis:";echo info | redis-cli  |grep connected_client;echo -n "php-fpm:";ps -ef |awk "/php-fpm: pool/" |grep -v awk |wc -l;echo -n "import:";ps aux | awk /import/ | grep -v awk | grep -v watch |wc -l'
+
