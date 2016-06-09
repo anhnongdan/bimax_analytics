e36d84fe9f02838620eb0d3609be470b6c8947da
Vu Tran <baysao@gmail.com>
2016-06-09 23:26:08 +0700

    add pipe

--- piwik_pipe.sh
+++ piwik_pipe.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-import=/usr/share/nginx/html/piwik
-ilogs=$import/piwik-analytics-scripts/import_logs.py
-id=$1
+import=/mnt/app/bimax_analytics 
+pw=/usr/share/nginx/html/piwik
+ilogs=$import/import_logs.py
+tk=126414e1cfe26bf6cc00a9a0e366c1f7
 opt=""
-if [ $id -eq 0 ];then
+if [ $1 -eq 0  ];then
 	opt="--add-sites-new-hosts"
 fi
 exec python $ilogs \
- --token-auth=12641baysao4e1cfe26bf6cc00a9a0e366c1f7\
- --config=$import/config/config.ini.php \
+ --token-auth=$tk \
+ --config=$pw/config/config.ini.php \
  --url=http://localhost/ $opt \
  --recorders=32 --recorder-max-payload-size=200 --enable-http-errors --enable-http-redirects --enable-static --enable-bots \
- --log-format-name=nginx_json -
- #--debug  --log-format-name=nginx_json -
+ --log-format-name=nginx_json - 
+ #--debug  --log-format-name=nginx_json - 
